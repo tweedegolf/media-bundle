c904f450a3ea216ec8b13029d76904f96eb4ef7d
Marlon Baeten <marlon@tweedegolf.com>
2014-08-12 12:02:41 +0200

    Initial commit

---
+++ src/TweedeGolf/MediaBundle/DependencyInjection/TweedeGolfMediaExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TweedeGolf\MediaBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class TweedeGolfMediaExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+
+    }
+}
---
+++ src/TweedeGolf/MediaBundle/TweedeGolfMediaBundle.php
@@ -0,0 +1,12 @@
+
+<?php
+
+namespace TweedeGolf\MediaBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TweedeGolfMediaBundle extends Bundle
+{
+
+}
