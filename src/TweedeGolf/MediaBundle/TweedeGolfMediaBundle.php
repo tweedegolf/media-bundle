a5335d182c464c33888a381b6a6dd7f61cb4a852
Massimiliano Arione <garakkio@gmail.com>
2016-03-22 13:31:54 +0100

    fix serializer File reference

--- src/TweedeGolf/MediaBundle/TweedeGolfMediaBundle.php
+++ src/TweedeGolf/MediaBundle/TweedeGolfMediaBundle.php
@@ -2,7 +2,6 @@
 
 namespace TweedeGolf\MediaBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TweedeGolfMediaBundle extends Bundle
